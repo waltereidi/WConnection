e8cec52cd586af7c48473a4a51e26a9e47dd8574
waltereidi <walter-eidi@hotmail.com>
2024-01-07 18:14:36 -0300

    Add project files.

---
+++ WConnectionKeyVault/KeyVault.cs
@@ -0,0 +1,13 @@
+﻿namespace WConnectionKeyVault
+{
+    public static class KeyVault
+    {
+        public static string GetConnectionString(this string? solution)
+        {
+            if (solution == "loja.Infra")
+                return "Server=MWALTR;Database=loja;Trusted_Connection=True;TrustServerCertificate=True;User Id=sa;Password=123";
+
+            return null;
+        }
+    }
+}
